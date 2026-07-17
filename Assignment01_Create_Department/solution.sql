1147e3a02ae40e4ce348ac3196cc7aa12d8e9fca
SASMITHA-1U25CA085 <sasmithasm62@gmail.com>
2026-07-17 12:45:48 +0530

    Update solution.sql

--- Assignment01_Create_Department/solution.sql
+++ Assignment01_Create_Department/solution.sql
@@ -1,3 +1,7 @@
+use collegeDBBCA;
+CREATE TABLE DEPARTMENT (department_ID int PRIMARY KEY,department_name VARCHAR(20),HOD VARCHAR(20));
+SELECT * FROM DEPARTMENT;
+
 -- Write your SQL here
 
 -- Create Database
